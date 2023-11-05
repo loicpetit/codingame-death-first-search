157a7a69eba87886b4e5d841a617ba5e0c7c532d
Loic PETIT <8918083+loicpetit@users.noreply.github.com>
2023-11-05 01:44:32 -0400

    Add tunnel path checks

--- src/main.go
+++ src/main.go
@@ -263,6 +263,102 @@ func getShortestPath(gameMap *GameMap, startIndex int, endIndex int) []int {
 	return indexes
 }
 
+func getTunnelPath(gameMap *GameMap, startIndex int, beforeEndIndex int, endIndex int) []int {
+	// debug("Get tunnel path between", startIndex, "and", endIndex)
+	if startIndex == endIndex {
+		return []int{startIndex}
+	}
+	nbNodes := len(gameMap.nodes)
+	parentIndex := make([]int, nbNodes)
+	valueIndex := make([]int, nbNodes)
+	visitedIndex := make([]bool, nbNodes)
+	for i := 0; i < nbNodes; i++ {
+		parentIndex[i] = -1
+		valueIndex[i] = 0
+		visitedIndex[i] = false
+	}
+	visitedIndex[beforeEndIndex] = true
+	parentIndex[beforeEndIndex] = endIndex
+	visitedIndex[endIndex] = true
+	valueIndex[endIndex] = getTunnelNodeValue(gameMap, beforeEndIndex, endIndex, endIndex)
+	var queue []int
+	queue = append(queue, beforeEndIndex)
+	for {
+		currentIndex := queue[0]
+		if currentIndex == startIndex {
+			break
+		}
+		// debug("current index", currentIndex)
+		queue = queue[1:]
+		currentNode := gameMap.nodes[currentIndex]
+		value := getTunnelNodeValue(gameMap, beforeEndIndex, endIndex, currentIndex)
+		valueIndex[currentIndex] = value
+		for _, linkedNode := range currentNode.links {
+			linkedNodeIndex := linkedNode.index
+			if linkedNodeIndex == endIndex {
+				continue
+			}
+			if parentIndex[linkedNodeIndex] == -1 ||
+				(parentIndex[linkedNodeIndex] != currentIndex &&
+					parentIndex[currentIndex] != linkedNodeIndex &&
+					valueIndex[parentIndex[linkedNodeIndex]] < value) {
+				parentIndex[linkedNodeIndex] = currentIndex
+			}
+			if !visitedIndex[linkedNodeIndex] {
+				visitedIndex[linkedNodeIndex] = true
+				queue = append(queue, linkedNodeIndex)
+			}
+		}
+		if len(queue) == 0 {
+			break
+		}
+	}
+	// if beforeEndIndex == 6 && endIndex == 0 {
+	// 	fmt.Fprint(os.Stderr, "Parents ")
+	// 	for i, v := range parentIndex {
+	// 		fmt.Fprint(os.Stderr, fmt.Sprintf("[%d:%d]", i, v))
+	// 	}
+	// 	fmt.Fprintln(os.Stderr)
+	// 	fmt.Fprint(os.Stderr, "Values ")
+	// 	for i, v := range valueIndex {
+	// 		fmt.Fprint(os.Stderr, fmt.Sprintf("[%d:%d]", i, v))
+	// 	}
+	// 	fmt.Fprintln(os.Stderr)
+	// }
+	if parentIndex[startIndex] == -1 {
+		return []int{}
+	}
+	// debug("Create indexes")
+	var indexes []int
+	currentParentIndex := startIndex
+	parentCount := 0
+	for parentIndex[currentParentIndex] != -1 {
+		parentCount++
+		// debug("append", currentParentIndex)
+		indexes = append(indexes, currentParentIndex)
+		currentParentIndex = parentIndex[currentParentIndex]
+		if parentCount == nbNodes {
+			return []int{}
+		}
+	}
+	indexes = append(indexes, endIndex)
+	// debug("Indexes ", indexes)
+	return indexes
+}
+
+func getTunnelNodeValue(gameMap *GameMap, beforeEndIndex int, endIndex int, index int) int {
+	if endIndex == index {
+		return 10000
+	}
+	if beforeEndIndex == index {
+		return 100000
+	}
+	if index < 0 || index >= len(gameMap.nodes) {
+		return 0
+	}
+	return gameMap.nodes[index].getNbLinkedExits()
+}
+
 func evaluateRisk(gameMap *GameMap, indexes []int) int {
 	if gameMap == nil || len(indexes) < 2 {
 		return 0
@@ -332,12 +428,25 @@ func evaluateTunnelRisk(gameMap *GameMap, indexes []int) int {
 	return nbConsecutiveLinkedExits
 }
 
-func getPathToExit(channel chan *Path, gameMap *GameMap, exitLink *Link) {
+func getShortestPathToExitLink(channel chan *Path, gameMap *GameMap, exitLink *Link) {
 	var indexes []int
 	if exitLink.node1.isExit {
 		indexes = append(getShortestPath(gameMap, gameMap.bobnetAgentIndex, exitLink.node2.index), exitLink.node1.index)
 	} else {
 		indexes = append(getShortestPath(gameMap, gameMap.bobnetAgentIndex, exitLink.node1.index), exitLink.node2.index)
+	}
+	risk := evaluateRisk(gameMap, indexes)
+	channel <- &Path{indexes: indexes, risk: risk}
+}
+
+func getTunnelPathToExitLink(channel chan *Path, gameMap *GameMap, exitLink *Link) {
+	var indexes []int
+	if exitLink.node1.isExit {
+		// debug("get tunnel from", gameMap.bobnetAgentIndex, "by", exitLink.node2.index, "to", exitLink.node1.index)
+		indexes = getTunnelPath(gameMap, gameMap.bobnetAgentIndex, exitLink.node2.index, exitLink.node1.index)
+	} else {
+		// debug("get tunnel from", gameMap.bobnetAgentIndex, "by", exitLink.node1.index, "to", exitLink.node2.index)
+		indexes = getTunnelPath(gameMap, gameMap.bobnetAgentIndex, exitLink.node1.index, exitLink.node2.index)
 	}
 	risk := evaluateRisk(gameMap, indexes)
 	channel <- &Path{indexes: indexes, risk: risk}
@@ -360,20 +469,25 @@ func getBobnetPath(gameMap *GameMap) (*Path, error) {
 		return nil, errors.New("game map is missing")
 	}
 	exitLinks := getAllExitLinks(gameMap)
+	// debug("Exit links:", exitLinks)
 	nbExitLinks := len(exitLinks)
-	pathChannel := make(chan *Path, nbExitLinks)
-	debug(nbExitLinks, "paths to compute")
+	nbPaths := 2 * nbExitLinks
+	debug(nbPaths, "paths to compute")
+	pathChannel := make(chan *Path, nbPaths)
+	defer close(pathChannel)
 	for i := 0; i < nbExitLinks; i++ {
-		go getPathToExit(pathChannel, gameMap, exitLinks[i])
+		go getShortestPathToExitLink(pathChannel, gameMap, exitLinks[i])
+		go getTunnelPathToExitLink(pathChannel, gameMap, exitLinks[i])
 	}
 	var path *Path
-	for i := 0; i < nbExitLinks; i++ {
+	for i := 0; i < nbPaths; i++ {
 		pathToExit := <-pathChannel
-		if pathToExit == nil || len(pathToExit.indexes) == 0 {
+		if pathToExit == nil || len(pathToExit.indexes) < 2 {
 			continue
 		}
 		debug("Possible path:", pathToExit)
-		if path == nil || pathToExit.risk > path.risk {
+		if path == nil || pathToExit.risk > path.risk ||
+			(pathToExit.risk == path.risk && len(pathToExit.indexes) < len(path.indexes)) {
 			path = pathToExit
 		}
 	}
